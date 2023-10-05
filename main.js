670ab0acb194140132e28e477d0ac3c257dbe9ed
Vickidiaz <vickidiaz42@gmail.com>
2023-10-05 10:59:33 -0700

    added and fixed a like button to each card. need function still

--- main.js
+++ main.js
@@ -4,37 +4,35 @@ function MainModule(listingsID = "#listings") {
   const listingsElement = document.querySelector(listingsID);
 //  This is the chnages I made for self assessment
 // notes to remember what I did: created card text class where I can add in/ bring in the data from json file
-  function getListingCode(listing) {
-    return `<div class="col-4">
-      <div class="listing card">
+function getListingCode(listing) {
+  return `<div class="col-4">
+    <div class="listing card">
+      <img
+        src="${listing.picture_url}"
+        class="card-img-top"
+        alt="AirBNB Listing"
+      />
+      // card body class: this is where I all elemnts will be addded. More specifically, in teh card-text part
+      <div class="card-body">
+        <h2 class="card-title">${listing.name}</h2>
+        <p class="card-text"><strong>Description: </strong>${listing.description}</p>
+        <p class="card-text"><strong>Amenities: </strong>${listing.amenities}</p>
+        <p class="card-text"><strong>Host: </strong>${listing.host_name}</p>
         <img
-          src="${listing.picture_url}"
-          class="card-img-top"
-          alt="AirBNB Listing"
+          src="${listing.host_picture_url}"
+          alt="${listing.host_name}'s Profile Picture"
+          class="host-picture"
         />
-        // craeted a card-body class, here all listing info will be added 
-        <div class="card-body">
-          <h2 class="card-title">${listing.name}</h2>
-          // use strong to be able to see the text difference
-          <p class="card-text"><strong>Description: </strong>${listing.description}</p>
-          <p class="card-text"><strong>Amenities: </strong>${listing.amenities}</p>
-          <p class="card-text"><strong>Host: </strong>${listing.host_name}</p>
-          <img
-            src="${listing.host_picture_url}"
-            alt="${listing.host_name}'s Profile Picture"
-            class="host-picture"
-          />
-          <p class="card-text"><strong>Price: </strong>${listing.price}</p>
-          <a href="${listing.listing_url}" class="btn btn-primary">View Listing</a>
-          // unique component: added a like button
-          // Create the "Like" button and add it to the card body
-          const likeButton = document.createElement('button');
-          likeButton.classList.add('btn', 'btn-primary', 'like-button');
-          likeButton.textContent = 'Like';
-        </div>
+        <p class="card-text"><strong>Price: </strong>${listing.price}</p>
+        <a href="${listing.listing_url}" class="btn btn-primary">View Listing</a>
+        // Like button added here
+        // added unique component 
+        <button class="btn btn-primary like-button">Like</button>
       </div>
-    </div>`;
-  }
+    </div>
+  </div>`;
+}
+
 
   function redraw(listings) {
     listingsElement.innerHTML = listings.map(getListingCode).join("\n");
